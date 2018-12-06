0bd0658f7a8bdc6c125e282e038543e4236db1b3
Vijay Bachu <vj7834@gmail.com>
2018-12-06 20:31:12 +0000

    added modded App.java

--- src/main/java/com/ambientideas/App.java
+++ src/main/java/com/ambientideas/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World from vijay again and again - Changed!" );
+        System.out.println( "Hello World from vijay vijay again and again - Changed!" );
     }
 }
